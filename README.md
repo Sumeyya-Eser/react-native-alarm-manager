536ceee56a7926b66c9ed378aa5f58464c2aa2c8
Sumeyya Eser <smeyyaeser@gmail.com>
2022-10-18 14:06:19 +0300

    feat: screen wake up works

---
+++ README.md
@@ -0,0 +1,338 @@
+# react-native-alarm-manager
+
+[![npm version](https://img.shields.io/npm/v/react-native-alarm-manager)](https://www.npmjs.org/package/react-native-alarm-manager)
+[![install size](https://packagephobia.com/badge?p=react-native-alarm-manager)](https://packagephobia.com/result?p=react-native-alarm-manager)
+![react-native](https://img.shields.io/badge/ReactNative->=0.60.0-61DAFB?logo=react)
+
+Alarm manager for React Native
+
+## Table of Contents
+
+  - [Installation](#installation)
+    - [Adding the package](#adding-the-package)
+      - [npm](#npm)
+      - [yarn](#yarn)
+    - [Manipulating codes in your project](#manipulating-codes-in-your-project)
+      - [Check android SDK](#check-android-sdk)
+      - [Register components](#register-components)
+      - [Overriding MainActivity methods](#overriding-mainactivity-methods)
+      - [Create resource directory](#create-resource-directory)
+  - [Usage](#usage)
+    - [Props](#props)
+    - [Alarm Scheduling](#alarm-scheduling)
+    - [Alarm Searching](#alarm-searching)
+      - [One](#one)
+      - [All](#all)
+    - [Alarm Modifying](#alarm-modifying)
+    - [Alarm Deleting](#alarm-deleting)
+    - [Alarm Stopping](#alarm-stopping)
+  - [Typescript](#typescript)
+  - [Example](#example)
+    - [Alarm scheduling from the main page](#alarm-scheduling-from-the-main-page)
+    - [Alarm searching from the top toolbar](#alarm-searching-from-the-top-toolbar)
+    - [Alarm modifying from the alarm list](#alarm-modifying-from-the-alarm-list)
+    - [Alarm deleting from the alarm list](#alarm-deleting-from-the-alarm-list)
+    - [Alarm toggling from the alarm list](#alarm-toggling-from-the-alarm-list)
+    - [Alarm stopping after activation](#alarm-stopping-after-activation)
+  - [License](#license)
+
+
+## Installation
+
+### Adding the package
+
+#### npm
+
+```bash
+$ npm install react-native-alarm-manager
+```
+
+#### yarn
+
+```bash
+$ yarn add react-native-alarm-manager
+```
+
+### Manipulating codes in your project
+
+#### Check android SDK
+
+This package is compiled with Android SDK Platform 29.  
+Your project SDK version doesn't matter.  
+Android SDK Platform 29 must be installed.
+
+#### Register components
+
+Go to AndroidManifest.xml and register the service and receiver.
+
+```xml
+<manifest ... >
+    <application ... >
+      <activity ... >
+      </activity>
+      
+      <!-- Add the following code -->
+      <service android:name="com.baekgol.reactnativealarmmanager.util.AlarmService" android:enabled="true" android:exported="false" />
+      <receiver android:name="com.baekgol.reactnativealarmmanager.util.AlarmReceiver" android:enabled="true" android:exported="false" />
+      <receiver android:name="com.baekgol.reactnativealarmmanager.util.BootReceiver" android:enabled="false" android:exported="false" >
+          <intent-filter android:priority="999">
+              <action android:name="android.intent.action.BOOT_COMPLETED" />
+          </intent-filter>
+      </receiver>
+      
+    </application>
+</manifest>
+```
+
+#### Overriding MainActivity methods
+
+Go to MainActivity.java and override the onCreate and createReactActivityDelegate methods as follows.  
+It can work properly during the process of calling parameters and rebooting the android with alarm functions.
+
+```java
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+
+  ComponentName receiver = new ComponentName(this, BootReceiver.class);
+  PackageManager packageManager = this.getPackageManager();
+
+  packageManager.setComponentEnabledSetting(receiver,
+          PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+          PackageManager.DONT_KILL_APP);
+}
+
+@Override
+protected ReactActivityDelegate createReactActivityDelegate() {
+  return new ReactActivityDelegate(this, getMainComponentName()){
+    @Nullable
+    @Override
+    protected Bundle getLaunchOptions() {
+      Intent intent = getIntent();
+      Bundle bundle = intent.getExtras();
+
+      if(intent.getBooleanExtra("notiRemovable", true))
+        AlarmModule.stop(this.getContext());
+
+      return bundle;
+    }
+  };
+}
+```
+
+#### Create resource directory
+
+Configure your project's resource directory.  
+This is a necessary process to apply the alarm sound and notification icon.
+
+```
+project/app/src/main/res/raw       // alarm sound
+project/app/src/main/res/drawable  // notification icon
+```
+
+## Usage
+
+First, import the module to use the alarm function.
+
+```javascript
+import Alarm from 'react-native-alarm-manager';
+```
+
+### Props
+
+|Prop|Type|Description|Note|
+|-|-|-|-|
+|**alarm_id**|Number|Unique value of an alarm.|Auto Increment|
+|**alarm_time**|String|Value to set the alarm time.|HH:mm:00|
+|**alarm_title**|String|Title of notification.|Nullable|
+|**alarm_text**|String|Text of notification.|Nullable|
+|**alarm_sound**|String|Sound that rings when the alarm is activated.|Exclude file extensions|
+|**alarm_icon**|String|Icon of notification.|Exclude file extensions|
+|**alarm_sound_loop**|Boolean|Value to set whether the alarm sounds repeatedly.|Default: true|
+|**alarm_vibration**|Boolean|Value to set whether the alarm will vibrate when it is activated.|Default: true|
+|**alarm_noti_removable**|Boolean|Value to set whether to end the alarm when an notification is clicked.|Default: true|
+|**alarm_activate**|Boolean|Value to set whether to activate the alarm.|Default: true|
+
+### Alarm Scheduling
+
+This schedules an alarm.
+Make sure that alarm_time must be in HH:mm:00 format.
+* Hour(00 ~ 23)
+* Minute(00 ~ 59)
+* Second(00)
+
+```javascript
+const alarm = {
+  alarm_time: 12:30:00,   // HH:mm:00
+  alarm_title: 'title',
+  alarm_text: 'text',
+  alarm_sound: 'sound',   // sound.mp3
+  alarm_icon: 'icon',     // icon.png
+  alarm_sound_loop: true,
+  alarm_vibration: true,
+  alarm_noti_removable: true,
+  alarm_activate: true
+};
+
+Alarm.schedule(
+  alarm,
+  success => console.log(success),  // success message
+  fail => console.log(fail)         // fail message
+);
+```
+
+### Alarm Searching
+
+Alarm searching is provided in two ways.
+
+#### One
+
+This searches for an alarm.  
+You can access the alarm_id to obtain alarm information for that ID.
+
+```javascript
+Alarm.search(
+  id,
+  success => console.log(success),  // alarm
+  fail => console.log(fail)         // fail message
+);
+```
+
+#### All
+
+This searches for all alarms.
+
+```javascript
+Alarm.searchAll(
+  success => console.log(success),  // alarm list
+  fail => console.log(fail)         // fail message
+);
+```
+
+### Alarm Modifying
+
+This is almost identical to alarm scheduling, but must be additionally given alarm_id.    
+Alarm information for that ID will be changed.  
+If you want an alarm toggle, just change the alarm_activate.
+
+```javascript
+const alarm = {
+  alarm_id: 3,
+  alarm_time: 15:27:00,   // HH:mm:00
+  alarm_title: 'title modify',
+  alarm_text: 'text modify',
+  alarm_sound: 'sound3',  // sound.mp3
+  alarm_icon: 'icon2',    // icon.png
+  alarm_sound_loop: false,
+  alarm_vibration: true,
+  alarm_noti_removable: false,
+  alarm_activate: true    // value for alarm toggle
+};
+
+Alarm.modify(
+  alarm,
+  success => console.log(success),  // success message
+  fail => console.log(fail)         // fail message
+);
+```
+
+### Alarm Deleting
+
+This deletes the alarm for that ID.  
+This also automatically cancels the alarm schedule.
+
+```javascript
+Alarm.delete(
+  id,
+  success => console.log(success),  // success message
+  fail => console.log(fail)         // fail message
+);
+```
+
+### Alarm Stopping
+
+This turns off the alarm.  
+This means that the sound stops and the notification disappears.
+
+```javascript
+Alarm.stop(
+  success => console.log(success),  // success message
+  fail => console.log(fail)         // fail message
+);
+```
+
+## Typescript 
+Typescript declaration types has been added in version 1.2.0, it provides a set of convenient types that can be used while writing a typescript RN app.
+
+```typescript
+import Alarm, {AlarmScheduleType} from 'react-native-alarm-manager';
+
+const alarm: AlarmScheduleType = {
+  alarm_time: '15:27:00', // HH:mm:00
+  alarm_title: 'title',
+  alarm_text: 'text',
+  alarm_sound: 'sound', // sound.mp3
+  alarm_icon: 'icon', // icon.png
+  alarm_sound_loop: false,
+  alarm_vibration: true,
+  alarm_noti_removable: false,
+  alarm_activate: true, // value for alarm toggle
+};
+
+Alarm.schedule(
+  alarm,
+  success => console.log(success),
+  fail => console.log(fail),
+);
+```
+```typescript
+import Alarm, {AlarmType} from 'react-native-alarm-manager';
+
+const alarm: AlarmType = {
+  alarm_id: 3,
+  alarm_time: '15:27:00', // HH:mm:ss
+  alarm_title: 'title modify',
+  alarm_text: 'text modify',
+  alarm_sound: 'sound3', // sound3.mp3
+  alarm_icon: 'icon2', // icon2.png
+  alarm_sound_loop: false,
+  alarm_vibration: true,
+  alarm_noti_removable: false,
+  alarm_activate: true, // value for alarm toggle
+};
+
+Alarm.modify(
+  alarm,
+  success => console.log(success),
+  fail => console.log(fail),
+);
+```
+## Example
+
+### Alarm scheduling from the main page
+
+![alarm-scheduling](images/alarm-scheduling.gif)
+
+### Alarm searching from the top toolbar
+
+![alarm-searching](images/alarm-searching.gif)
+
+### Alarm modifying from the alarm list
+
+![alarm-modifying](images/alarm-modifying.gif)
+
+### Alarm deleting from the alarm list
+
+![alarm-deleting](images/alarm-deleting.gif)
+
+### Alarm toggling from the alarm list
+
+![alarm-toggling](images/alarm-toggling.gif)
+
+### Alarm stopping after activation
+
+![alarm-stopping](images/alarm-stopping.gif)
+
+## License
+
+[MIT](LICENSE)
---
+++ android/src/main/java/com/sea/nativealarm/AlarmModule.java
@@ -0,0 +1,377 @@
+package com.sea.nativealarm;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.bridge.ReadableMap;
+import com.sea.nativealarm.db.Database;
+import com.sea.nativealarm.model.AlarmDto;
+import com.sea.nativealarm.util.AlarmReceiver;
+import com.sea.nativealarm.util.AlarmService;
+
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+
+import java.sql.Time;
+
+
+public class AlarmModule extends ReactContextBaseJavaModule {
+  private ReactApplicationContext reactContext;
+  private NotificationManager notificationManager;
+  private AlarmManager alarmManager;
+  private AlarmDto alarm;
+  private AlarmDto[] alarms;
+  private int affectedCnt;
+  private final String channelId = "alarm";
+
+  AlarmModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+    alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
+    createNotificationChannel();
+  }
+
+  @Override
+  public String getName() {
+    return "Alarm";
+  }
+
+  private void createNotificationChannel(){
+    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+      notificationManager = reactContext.getSystemService(NotificationManager.class);
+
+      NotificationChannel channel = new NotificationChannel(channelId, "Alarm", NotificationManager.IMPORTANCE_HIGH);
+      channel.setDescription("Alarm");
+      channel.setSound(null, null);
+      notificationManager.createNotificationChannel(channel);
+    }
+  }
+
+  @ReactMethod
+  public void schedule(final ReadableMap rm, Callback success, Callback fail){
+    Runnable r = new Runnable() {
+      @Override
+      public void run() {
+        Database db = Database.getInstance(getReactApplicationContext());
+        AlarmDto newAlarm = createAlarm(rm, false);
+        AlarmDto existedAlarm = db.alarmDao().search(newAlarm.getAlarmTime());
+
+        if(existedAlarm==null){
+          long alarmId = db.alarmDao().add(newAlarm);
+          alarm = db.alarmDao().search((int)alarmId);
+
+          String time = alarm.getAlarmTime().toString();
+          String[] timeInfo = time.split(":");
+          int hour = Integer.parseInt(timeInfo[0]);
+          int minute = Integer.parseInt(timeInfo[1]);
+          int second = Integer.parseInt(timeInfo[2]);
+
+          Calendar calendar = Calendar.getInstance();
+          calendar.setTimeInMillis(System.currentTimeMillis());
+
+          int currHour = calendar.get(Calendar.HOUR_OF_DAY);
+          int currMinute = calendar.get(Calendar.MINUTE);
+          int currSecond = calendar.get(Calendar.SECOND);
+
+          calendar.set(Calendar.HOUR_OF_DAY, hour);
+          calendar.set(Calendar.MINUTE, minute);
+          calendar.set(Calendar.SECOND, 0);
+
+          if(hour<currHour || (hour==currHour && minute<currMinute) || (hour==currHour && minute==currMinute && second<currSecond))
+            calendar.setTimeInMillis(calendar.getTimeInMillis() + (1000*60*60*24));
+
+          Intent alarmIntent = new Intent(reactContext, AlarmReceiver.class);
+          alarmIntent.putExtra("id", alarm.getAlarmId());
+          alarmIntent.putExtra("hour", hour);
+          alarmIntent.putExtra("minute", minute);
+          alarmIntent.putExtra("title", alarm.getAlarmTitle());
+          alarmIntent.putExtra("text", alarm.getAlarmText());
+          alarmIntent.putExtra("sound", alarm.getAlarmSound());
+          alarmIntent.putExtra("icon", alarm.getAlarmIcon());
+          alarmIntent.putExtra("soundLoop", alarm.isAlarmSoundLoop());
+          alarmIntent.putExtra("vibration", alarm.isAlarmVibration());
+          alarmIntent.putExtra("notiRemovable", alarm.isAlarmNotiRemovable());
+
+          PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(reactContext, alarm.getAlarmId(), alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+          alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmPendingIntent);
+        }
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      if(alarm!=null) success.invoke("Alarm scheduling was successful.");
+      else fail.invoke("An alarm set for this time already exists.");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while scheduling the alarm.");
+    }finally{
+      alarm = null;
+    }
+  }
+
+  @ReactMethod
+  public void search(final int id, Callback success, Callback fail){
+    Runnable r = new Runnable() {
+      @Override
+      public void run() {
+        Database db = Database.getInstance(getReactApplicationContext());
+        alarm = db.alarmDao().search(id);
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      if(alarm!=null) success.invoke(createMap(alarm));
+      else fail.invoke("An error occurred while searching the alarm.");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while searching the alarm.");
+    }finally{
+      alarm = null;
+    }
+  }
+
+  @ReactMethod
+  public void searchAll(Callback success, Callback fail){
+    Runnable r = new Runnable() {
+      @Override
+      public void run() {
+        Database db = Database.getInstance(getReactApplicationContext());
+        alarms = db.alarmDao().searchAll();
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      success.invoke(createArray(alarms));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while searching alarms.");
+    }finally{
+      alarms = null;
+    }
+  }
+
+  @ReactMethod
+  public void modify(final ReadableMap rm, Callback success, Callback fail){
+    Runnable r = new Runnable(){
+      @Override
+      public void run(){
+        Database db = Database.getInstance(getReactApplicationContext());
+        AlarmDto newAlarm = createAlarm(rm, true);
+        AlarmDto existedAlarm = db.alarmDao().search(newAlarm.getAlarmId());
+        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(
+                reactContext,
+                newAlarm.getAlarmId(),
+                new Intent(reactContext, AlarmReceiver.class),
+                PendingIntent.FLAG_NO_CREATE);
+
+        if(existedAlarm!=null && newAlarm.getAlarmId()==existedAlarm.getAlarmId()){
+          if(alarmPendingIntent!=null){
+            alarmPendingIntent.cancel();
+            alarmManager.cancel(alarmPendingIntent);
+          }
+
+          affectedCnt = db.alarmDao().modifyAlarm(newAlarm);
+
+          if(newAlarm.isAlarmActivate()){
+            String time = newAlarm.getAlarmTime().toString();
+            String[] timeInfo = time.split(":");
+            int hour = Integer.parseInt(timeInfo[0]);
+            int minute = Integer.parseInt(timeInfo[1]);
+            int second = Integer.parseInt(timeInfo[2]);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+
+            int currHour = calendar.get(Calendar.HOUR_OF_DAY);
+            int currMinute = calendar.get(Calendar.MINUTE);
+            int currSecond = calendar.get(Calendar.SECOND);
+
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+            calendar.set(Calendar.SECOND, 0);
+
+            if(hour<currHour || (hour==currHour && minute<currMinute) || (hour==currHour && minute==currMinute && second<currSecond))
+              calendar.setTimeInMillis(calendar.getTimeInMillis() + (1000*60*60*24));
+
+            Intent alarmIntent = new Intent(reactContext, AlarmReceiver.class);
+            alarmIntent.putExtra("id", newAlarm.getAlarmId());
+            alarmIntent.putExtra("hour", hour);
+            alarmIntent.putExtra("minute", minute);
+            alarmIntent.putExtra("title", newAlarm.getAlarmTitle());
+            alarmIntent.putExtra("text", newAlarm.getAlarmText());
+            alarmIntent.putExtra("sound", newAlarm.getAlarmSound());
+            alarmIntent.putExtra("icon", newAlarm.getAlarmIcon());
+            alarmIntent.putExtra("soundLoop", newAlarm.isAlarmSoundLoop());
+            alarmIntent.putExtra("vibration", newAlarm.isAlarmVibration());
+            alarmIntent.putExtra("notiRemovable", newAlarm.isAlarmNotiRemovable());
+
+            alarmPendingIntent = PendingIntent.getBroadcast(reactContext, newAlarm.getAlarmId(), alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmPendingIntent);
+          }
+        }
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      if(affectedCnt!=0) success.invoke("Alarm modification was successful.");
+      else fail.invoke("An error occurred while modifying the alarm.");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while modifying the alarm.");
+    }finally{
+      affectedCnt = 0;
+    }
+  }
+
+  @ReactMethod
+  public void delete(final int id, Callback success, Callback fail){
+    Runnable r = new Runnable(){
+      @Override
+      public void run(){
+        Database db = Database.getInstance(getReactApplicationContext());
+        PendingIntent alarmPendingIntent = PendingIntent.getBroadcast(
+                reactContext,
+                id,
+                new Intent(reactContext, AlarmReceiver.class),
+                PendingIntent.FLAG_NO_CREATE);
+
+        if(alarmPendingIntent!=null) {
+          alarmPendingIntent.cancel();
+          alarmManager.cancel(alarmPendingIntent);
+        }
+
+        affectedCnt = db.alarmDao().deleteAlarm(id);
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      if(affectedCnt!=0) success.invoke("Alarm deleting was successful.");
+      else fail.invoke("An error occurred while deleting the alarm.");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while deleting the alarm.");
+    }finally{
+      affectedCnt = 0;
+    }
+  }
+
+  @ReactMethod
+  public void stop(Callback success, Callback fail){
+    Runnable r = new Runnable(){
+      @Override
+      public void run(){
+        Intent alarmServiceIntent = new Intent(reactContext, AlarmService.class);
+        reactContext.stopService(alarmServiceIntent);
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+      success.invoke("Alarm stopping was successful.");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+      fail.invoke("An error occurred while stopping the alarm.");
+    }
+  }
+
+  public static void stop(final Context context){
+    Runnable r = new Runnable(){
+      @Override
+      public void run(){
+        Intent alarmServiceIntent = new Intent(context, AlarmService.class);
+        context.stopService(alarmServiceIntent);
+      }
+    };
+
+    Thread thread = new Thread(r);
+    thread.start();
+
+    try{
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  WritableMap createMap(AlarmDto dto){
+    DateFormat format = new SimpleDateFormat("HH:mm:ss");
+    WritableMap wm = new WritableNativeMap();
+
+    wm.putInt("alarm_id", dto.getAlarmId());
+    wm.putString("alarm_time", format.format(dto.getAlarmTime().getTime()));
+    wm.putString("alarm_title", dto.getAlarmTitle());
+    wm.putString("alarm_text", dto.getAlarmText());
+    wm.putString("alarm_sound", dto.getAlarmSound());
+    wm.putString("alarm_icon", dto.getAlarmIcon());
+    wm.putBoolean("alarm_sound_loop", dto.isAlarmSoundLoop());
+    wm.putBoolean("alarm_vibration", dto.isAlarmVibration());
+    wm.putBoolean("alarm_noti_removable", dto.isAlarmNotiRemovable());
+    wm.putBoolean("alarm_activate", dto.isAlarmActivate());
+
+    return wm;
+  }
+
+  WritableArray createArray(AlarmDto[] dtos){
+    WritableArray wa = new WritableNativeArray();
+
+    for(AlarmDto dto:dtos)
+      wa.pushMap(createMap(dto));
+
+    return wa;
+  }
+
+  AlarmDto createAlarm(ReadableMap rm, boolean isModify){
+    AlarmDto newAlarm = new AlarmDto();
+
+    if(isModify) newAlarm.setAlarmId(rm.getInt("alarm_id"));
+    newAlarm.setAlarmTime(Time.valueOf(rm.getString("alarm_time")));
+    newAlarm.setAlarmTitle(rm.getString("alarm_title"));
+    newAlarm.setAlarmText(rm.getString("alarm_text"));
+    newAlarm.setAlarmSound(rm.getString("alarm_sound"));
+    newAlarm.setAlarmIcon(rm.getString("alarm_icon"));
+    newAlarm.setAlarmSoundLoop(rm.getBoolean("alarm_sound_loop"));
+    newAlarm.setAlarmVibration(rm.getBoolean("alarm_vibration"));
+    newAlarm.setAlarmNotiRemovable(rm.getBoolean("alarm_noti_removable"));
+    newAlarm.setAlarmActivate(rm.getBoolean("alarm_activate"));
+
+    return newAlarm;
+  }
+}
